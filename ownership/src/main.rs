47f303736353d6d2c441ec637b4b3b143745935a
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2022-09-14 19:59:13 +0200

    reference and borrowing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn ownership_examples() {
     let mut s = String::from("hello");
     s.push_str(", world!");
     println!("{}", s);
@@ -24,6 +24,31 @@ fn main() {
     println!("the length of '{}' is {}.", s4, len);
 }
 
+fn reference_and_borrowing_examples() {
+    let s1 = String::from("hello");
+    let len = calculate_length_from_ref(&s1); // reference borrowing. value of s1 is not dropped
+    println!("The length from reference of '{}' is {}.", s1, len);
+    // Rust prevents more than one mutable reference. to work around it, create a new scope
+    let mut s = String::from("Yannick");
+    {
+        let r1 = &mut s;
+        r1.push_str(" from the inner scope");
+        println!("{} **", r1);
+        // safely use r1 here
+    } // r1 goes out of scope here
+    let r2 = &mut s;
+    println!("{}", r2);
+    // let dangling_reference = dangle();
+    let valid_string = no_dangle();
+    // println!("{}", dangling_reference);
+    println!("{}", valid_string);
+}
+
+fn main() {
+    ownership_examples();
+    reference_and_borrowing_examples();
+}
+
 fn take_ownership(some_string: String) {
     println!("{}", some_string);
 }
@@ -46,3 +71,14 @@ fn calculate_length(s: String) -> (String, usize) {
     let len = s.len();
     (s, len)
 }
+
+fn calculate_length_from_ref(s: &String) -> usize {
+    s.len()
+}
+
+// fn dangle() -> &String {
+fn no_dangle() -> String {
+    let s = String::from("hello");
+    // &s
+    s
+} // Here s goes out of scope and is dropped. Danger!!
