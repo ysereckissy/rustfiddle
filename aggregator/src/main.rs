49c974c44f8b6339b2a97aaa385a22801f5cdb8b
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2022-09-20 14:59:10 +0200

    lifetime

---
+++ aggregator/src/main.rs
@@ -0,0 +1,32 @@
+use aggregator::{ Summary, Tweet };
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    let outer = String::from("long string is long");
+    let result;
+    {
+        let inner = String::from("inner string");
+        result = longest(outer.as_str(), inner.as_str());
+        println!("The longest string is {}", result);
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
