280d0d77eba828e5072a8ff227796463e91fc3cf
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2022-09-21 22:50:13 +0200

    read file content and start refactoring

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,31 @@
 use std::env;
+use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    println!("With the text: \n{contents}")
 }
+struct Config {
+    _query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments.");
+        }
+        Ok(Config {
+            _query: args[1].clone(),
+            file_path: args[2].clone()
+        })
+    }
+}
+
