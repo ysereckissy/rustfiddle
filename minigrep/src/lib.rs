44370f2c421e95d44742f15c2d9425de3f5d74f7
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2022-09-22 20:59:39 +0200

    continue implementation of minigrep

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,53 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments.");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone()
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+    Ok(())
+}
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line.trim());
+        }
+    }
+    return results;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        pick three. ";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,24 +8,8 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("With the text: \n{contents}")
-}
-struct Config {
-    _query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments.");
-        }
-        Ok(Config {
-            _query: args[1].clone(),
-            file_path: args[2].clone()
-        })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
-
